0e85c610238bd6daec96868e2fc8dd81f4d69d46
navaneeth03 <navaneeth03@TVMATP236509D.ad.infosys.com>
2019-10-10 15:57:38 +0530

    pullingo printout

--- test/src/test/cucumber.java
+++ test/src/test/cucumber.java
@@ -2,7 +2,7 @@ package test;
 
 public class cucumber {
 	public static void maim(String[] args) {
-		System.out.println("main job first job edited");
+		System.out.println("main job first job pullingo");
 	}
 
 }
