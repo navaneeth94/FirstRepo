6c3e6625a5b481a6829c6dff61ef2ea6a0f42c00
navaneeth03 <navaneeth03@TVMATP118457L.ad.infosys.com>
2020-02-09 11:38:42 +0530

    commit to branch 2

--- test/src/test/cucumber.java
+++ test/src/test/cucumber.java
@@ -4,6 +4,10 @@ public class cucumber {
 	public static void main(String[] args) {
 		System.out.println("main job first job pullingo");
 		System.out.println("hi perosnal lap");
+		System.out.println("new addition in branch 2")
+		System.out.println("infy infy");
+	}
+	public method(){
 		System.out.println("infy");
 	}
 }
