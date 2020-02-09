0530f2f749e58fb6a6dd24a109d2f76082349505
navaneeth03 <navaneeth03@TVMATP118457L.ad.infosys.com>
2020-02-09 18:30:39 +0530

    "be

--- test/src/test/cucumber.java
+++ test/src/test/cucumber.java
@@ -3,7 +3,7 @@ package test;
 public class cucumber {
 	public static void main(String[] args) {
 		System.out.println("main job first job pullingo");
-		System.out.println("hi perosnal lap");
+		System.out.println("hi perosnal laps");
 		System.out.println("new addition in branch 2");
 		System.out.println("infy");
 
