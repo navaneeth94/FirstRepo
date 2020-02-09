7ceec4372816bbc3781c58a6c0988f7dc304374c
navaneeth03 <navaneeth03@TVMATP118457L.ad.infosys.com>
2020-02-09 11:42:22 +0530

    "adding semi colon in cucumber"

--- test/src/test/cucumber.java
+++ test/src/test/cucumber.java
@@ -4,7 +4,7 @@ public class cucumber {
 	public static void main(String[] args) {
 		System.out.println("main job first job pullingo");
 		System.out.println("hi perosnal lap");
-		System.out.println("new addition in branch 2")
+		System.out.println("new addition in branch 2");
 		System.out.println("infy");
 
 	}
