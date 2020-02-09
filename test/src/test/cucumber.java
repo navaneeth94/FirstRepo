04a605d498dfc5b582fa593a75dcdd85f5e24474
navaneeth03 <navaneeth03@TVMATP118457L.ad.infosys.com>
2020-02-09 09:50:16 +0530

    infy lap commit

--- test/src/test/cucumber.java
+++ test/src/test/cucumber.java
@@ -3,7 +3,7 @@ package test;
 public class cucumber {
 	public static void main(String[] args) {
 		System.out.println("main job first job pullingo");
-		System.out.println("hi infy lap");
+		System.out.println("hi infy lap");;
 	}
 
 }
